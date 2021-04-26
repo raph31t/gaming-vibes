fd59c64cbc93c0b9db064a368118eedf5cef2510
Raphael TILLET <rtillet@free.fr>
2021-04-26 22:33:56 +0200

    login & logout V3

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import Navbar from '../components/Navbar'
 import { AuthContextProvider } from '../stores/authContext'
 import '../styles/globals.css'
-import AuthContext from '../stores/AuthContext'
+import AuthContext from '../stores/authContext'
 
 function MyApp({ Component, pageProps }) {
   return (
