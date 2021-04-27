6c4ac0d5a70f57bdd5b0d5cec7b788c6071dff18
Raphael TILLET <rtillet@free.Fr>
2021-04-27 13:00:05 +0200

    supermario function

--- functions/supermario.js
+++ functions/supermario.js
@@ -1,11 +1,11 @@
-exports.handler = async () => {
-    console.log('function ran ')
-
-    const data = { name ='mario', age: 35, job: 'plumber'}
-
-    //return response to browser
+exports.handler = async (event, context) => {
+    console.log('function ran')
+  
+    const data = { name: 'mario', age: 35, job: 'plumber' } 
+  
+    // return response to browser
     return {
-        statusCode: 200,
-        body: JSON.stringify(data)
+      statusCode: 200,
+      body: JSON.stringify(data)
     }
-}
+  }
